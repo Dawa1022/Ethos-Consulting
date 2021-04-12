1ed15aec0e563de15dfb0273a259f97712a4aa6d
Dawa Sherpa <30375372+Dawa1022@users.noreply.github.com>
2021-04-12 19:47:32 -0400

    FAQ and Contact Page

--- docs/active.js
+++ docs/active.js
@@ -7,3 +7,23 @@ window.addEventListener("scroll", function() {
       sticky_header.classList.remove("is-sticky");
     }
   });
+
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
